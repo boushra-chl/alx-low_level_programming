4d1c2990ab7e4351dda07deacba21ceac5dce305
boushra-chl <bchilouch1998@gmail.com>
2023-04-22 08:17:41 -0700

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0 (Suceess)
+ */
+	int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+{
+	putchar(48+n);
+}
+	putchar('\n');
+	return (0);
+}
