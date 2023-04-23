8f57cc2cc429ad8c5993895eb0ad5ad44095562e
boushra-chl <bchilouch1998@gmail.com>
2023-04-22 23:30:49 -0700

    print single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0 (Suceess)
+ */
+	int main(void)
+{
+	int n;
+	
+	for (n = 0 ; n <= 9 ; n++)
+{
+	putchar(48 + n);
+	if (n == 9)
+	{
+		continue;
+	}	
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
