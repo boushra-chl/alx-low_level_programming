77905fd58a685db21818ef7c8abb79981f2e75c8
boushra-chl <bchilouch1998@gmail.com>
2023-04-23 03:19:38 -0700

    print combinations 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,33 +3,33 @@
  * main - A program that prints the alphabet in lowercase
  * Return: 0 (Suceess)
  */
-        int main(void)
+	int main(void)
 {
-        int n;
-        int m;
+	int n;
+	int m;
 	int d;
 
-        for (n = 0 ; n <= 8 ; n++)
+	for (n = 0 ; n <= 8 ; n++)
 {
-        	for (m = n + 1 ; m <= 9 ; m++)
-        {
-			for (d = m+1 ; d <= 9 ; d++)
+		for (m = n + 1 ; m <= 9 ; m++)
 		{
-                	if (n != m && n != d && m != d)
-                	{
-                        	putchar(48 + n);
-                        	putchar(48 + m);
+			for (d = m+1 ; d <= 9 ; d++)
+			{
+				if (n != m && n != d && m != d)
+				{
+				putchar(48 + n);
+				putchar(48 + m);
 				putchar(48 + d);
-                	}
-                	if (n == 7 && m == 8 && d == 9)
-                	{
-                        	continue;
-                	}
+				}
+				if (n == 7 && m == 8 && d == 9)
+				{
+				continue;
+				}
 			putchar(',');
 			putchar(' ');
+			}
 		}
-        }
 }
-        putchar('\n');
-        return (0);
+			putchar('\n');
+	return (0);
 }
