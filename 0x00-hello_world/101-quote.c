422d56ab0edff02d7883f704fe3f94fc7debc953
boushra-chl <bchilouch1998@gmail.com>
2023-04-24 04:35:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - A program that prints a line using the function printf function
  * Return: 1 (Success)
  */
-        int main(void)
+	int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar,
+		2015-10-19\n", stdout);
 	return (1);
 }
