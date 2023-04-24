8ec761271959cabf6386ceb1785928d3d9cfabff
boushra-chl <bchilouch1998@gmail.com>
2023-04-24 03:55:54 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the function printf function
+ * Return: 0 (Success)
+ */
+        int main(void)
+{
+	char ch;
+
+	char = ("and that piece of art is useful\" - Dora Korpar,");
+	putchar(ch);
+	putchar('\n');
+	return (1);
+}
