f611070cdca67720bbbbfd302c049c74c983a591
boushra-chl <bchilouch1998@gmail.com>
2023-04-25 09:37:01 -0700

    print alphabet in lower case three times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet_x10 - Function that prints ten times
+ * the alphabet in lowercase
+ * Return: void
+ */
+#include "main.h"
+	void print_alphabet_x10(void)
+{
+	int n;
+	char c;
+
+	for (n = 0 ; n <=9 ; n++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+	}
+}
