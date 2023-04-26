f9d572a986b9e30184f21467b14151e22e3215eb
boushra-chl <bchilouch1998@gmail.com>
2023-04-26 06:47:55 -0700

    add two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 /**
  * add - function that adds two integers and returns the result
- * Retutn: integer value result of addition (Success)
+ * Return: integer value result of addition (Success)
  * @a: integer to be added
  * @b: integer to be added
  */
