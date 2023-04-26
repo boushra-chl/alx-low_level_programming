76b389a307d254f838aaa5b2328456de2e336d92
boushra-chl <bchilouch1998@gmail.com>
2023-04-26 10:53:55 -0700

    sum of multiples of 3 and 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,9 @@
 /**
- * multiples_3_5 -  function that prints multiples of 3 and 5 below 1024
+ * main -  function that prints multiples of 3 and 5 below 1024
  * Return: integer, sum of all multiples
  */
-#include "main.h"
 #include <stdio.h>
-	int multiples_3_5(void)
+	int main(void)
 {
 	int n, p, q, sum;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,3 @@ int print_last_digit(int);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
-int multiples_3_5(void);
