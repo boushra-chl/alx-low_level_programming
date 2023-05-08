4324a2a5a137e3a641cdcb27afca012fa1e45898
boushra-chl <bchilouch1998@gmail.com>
2023-05-08 15:59:42 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @n: size of memory area to be filled
  * @s: pointer to memory area
  * @b: constant char
+ * Return: pointer to s
  */
 #include "main.h"
  char *_memset(char *s, char b, unsigned int n)
