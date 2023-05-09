a01f7983db2efcb5d687173cd666353808ad280c
boushra-chl <bchilouch1998@gmail.com>
2023-05-09 06:32:31 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+/**
+ * _strstr - locates a substring
+ * @haystack: string that we are looking in
+ * @needle: string to be looked for
+ * Return: pointer to haystack
+ */
+#include "main.h"
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+
+	while (*haystack)
+	{
+		for (i = 0 ; needle[i] ; i++)
+		{
+			if (needle[i] == *haystack)
+			{
+				return (haystack);
+			}
+		i++;
+		}
+	haystack++;
+	}
+return ('\0');
+}
