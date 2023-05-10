6dc2fe036704f3fb6d016c3727bcaed4701d2820
boushra-chl <bchilouch1998@gmail.com>
2023-05-10 20:07:30 +0000

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,5 @@
 /**
- * function that returns the length of a string
+ * _strlen_recursion - function that returns the length of a string
  * @s: char
  * Return: integer, length of string
  */
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 	{
 		return (0);
-	}	
+	}
 	else
 	{
 		return(1 + _strlen_recursion(s + 1));
