6dabd85f73d318e2c2378443f3989b618dcd0efb
boushra-chl <bchilouch1998@gmail.com>
2023-05-14 14:39:59 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+/**
+ * main - function that prints a program name
+ * @argc: integer size of command line arguments
+ * @argv: char, pointer to command line arguments
+ * Return: 1 or 0 (Success)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1 ; i < argc ; i++)
+	{
+		if (*argv[i] < 48 || *argv[i] > 57)
+		{
+			printf("Error\n");
+			return (1);
+			break;
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
