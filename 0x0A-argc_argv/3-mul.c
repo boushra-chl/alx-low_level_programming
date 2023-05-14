cb14a7578d448f01b90b19f6c0b718be8f538588
boushra-chl <bchilouch1998@gmail.com>
2023-05-14 13:51:12 +0000

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+/**
+ * main - function that prints a program name
+ * @argc: integer size of command line arguments
+ * @argv: char, pointer to command line arguments
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[])
+{
+	int mul;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (mul);
+}
