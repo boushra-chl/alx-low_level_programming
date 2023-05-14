fa2a8e95e21bec904b697e396ac8c1fc9b509756
boushra-chl <bchilouch1998@gmail.com>
2023-05-14 11:45:24 +0000

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
-/** main - function that prints a program name
+/**
+ * main - function that prints a program name
  * @argc: integer size of command line arguments
  * @argv: char, pointer to command line arguments
  * Return: 0 (Success)
  */
-
 #include <stdio.h>
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	argc = 0;
 
