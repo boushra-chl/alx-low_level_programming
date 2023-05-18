1043a64e9ad7f8d1dd30684da55cb5bdacba2ccc
boushra-chl <bchilouch1998@gmail.com>
2023-05-18 16:07:51 +0000

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,11 +39,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 		free(s);
-	}	
+	}
 		for (i = 0 ; i < size1 ; i++)
 		{
 			s[i] = s1[i];
-		}	
+		}
 		for (j = 0 ; j < n ; j++, i++)
 		{
 			s[i] = s2[j];
