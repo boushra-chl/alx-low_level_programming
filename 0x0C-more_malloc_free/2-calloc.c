d854ce3c4a4447038cb9c02c9346dd610db2cde2
boushra-chl <bchilouch1998@gmail.com>
2023-05-18 14:24:08 +0000

    concatenate strings

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+/**
+ * _calloc - function that allocates memory for an array using malloc 
+ * @nmemb: number of elements of the array
+ * @size: number of bytes 
+ * Return: pointer to the allocated memory or NULL if fails
+ */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
