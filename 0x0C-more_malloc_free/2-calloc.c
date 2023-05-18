69cf59a077d9602bd20df31d6d9f4ce353b5c599
boushra-chl <bchilouch1998@gmail.com>
2023-05-18 20:26:09 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0) 
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc (size * nmemb);
 	if (p == NULL)
