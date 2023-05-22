db0376e55c28e5270940f57fc2797ad0fd37c619
boushra-chl <bchilouch1998@gmail.com>
2023-05-22 20:28:52 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
 int _strlen (char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -28,6 +29,7 @@ int _strlen (char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
