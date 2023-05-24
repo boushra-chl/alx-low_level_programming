1d318843ce4ad13d8914a2b09dbc7198b8fbf703
boushra-chl <bchilouch1998@gmail.com>
2023-05-24 10:55:57 +0000

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
+void print_elem_hex(int elem);
+void print_elem(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
+
