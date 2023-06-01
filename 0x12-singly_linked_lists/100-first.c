ab4e021a47e33b62370473e738634d371067423d
boushra-chl <bchilouch1998@gmail.com>
2023-06-01 22:14:44 +0000

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_before_main - function that prints before main is executed
+ *
+ * Return: void
+ */
 void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
