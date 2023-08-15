7fa6e517569e57496a9c8a0027bd0c5618273db3
boushra-chl <bchilouch1998@gmail.com>
2023-08-15 19:04:59 +0000

    dog structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+redme in my directory
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog owner
+ *
+ * Description: dog structure
+ */
+struct dog
+{
+	char *name;
+        float age;
+	char *owner;
+};
+
