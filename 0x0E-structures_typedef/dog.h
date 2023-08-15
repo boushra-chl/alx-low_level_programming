fdc4fea3b18d53076f1a1d65eaa36029abfb82e6
boushra-chl <bchilouch1998@gmail.com>
2023-08-15 21:02:23 +0000

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,8 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(const char *str);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
