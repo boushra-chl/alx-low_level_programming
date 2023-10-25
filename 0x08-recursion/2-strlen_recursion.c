e70edabc867fd71e87dff1abc5a5f38c444f5c77
boushra-chl <bchilouch1998@gmail.com>
2023-10-25 21:31:38 +0000

    count length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,10 +11,11 @@
 int _strlen_recursion(char *s)
 {
 	int count = 0;
-	if (*s == '\0')
+
+	if (*s != '\0')
 	{
-		return(count);
+		count++;
+		count += _strlen_recursion(s + 1);
 	}
-	count++;
-	_strlen_recursion(s + 1)
+	return(count);
 }
