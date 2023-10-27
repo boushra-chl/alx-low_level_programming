e4252626f0e7c056b5c9281ca039aec9550175fb
boushra-chl <bchilouch1998@gmail.com>
2023-10-27 13:04:10 +0000

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * * main - program thet prints it's name
+ * * @argc: number of arguments
+ * * @argv: array of arguments
+ * *
+ * * Return: 0 Always
+ * */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	while (argv[argc])
+	{
+		argc++;
+	}
+	if(argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (0);
+}
