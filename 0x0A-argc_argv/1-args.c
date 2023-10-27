cbfe1fa9cecb153bc57b65371ca9e274be1113be
boushra-chl <bchilouch1998@gmail.com>
2023-10-27 12:41:50 +0000

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * * main - program thet prints it's name
+ * * @argc: number of arguments
+ * * @argv: array of arguments
+ * *
+ * * Return: 0 Always
+ * */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	while (argv[argc])
+	{
+		argc++;
+	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
