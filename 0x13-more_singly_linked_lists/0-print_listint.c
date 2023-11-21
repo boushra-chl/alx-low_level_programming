2e090f0b613e3998c84376216de8b1c5c30aa173
boushra-chl <bchilouch1998@gmail.com>
2023-11-21 08:04:26 +0000

    print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,27 +11,15 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node = 0;
+
 	if (h == NULL)
 		node = 0;
 	while (h != NULL)
 	{
-		_putchar(h->n + '0');
-		_putchar('\n');
+		printf("%d\n", h->n);
 		h = h->next;
 		node++;
 	}
 	return (node);
-}
-
-/**
- * * _putchar - writes the character c to stdout
- * * @c: The character to print
- * *
- * * Return: On success 1.
- * * On error, -1 is returned, and errno is set appropriately.
- * */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
 
