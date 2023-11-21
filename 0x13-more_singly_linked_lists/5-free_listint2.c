2defed2dae4904124bb9a50da8566bc86d417ba0
boushra-chl <bchilouch1998@gmail.com>
2023-11-21 10:01:01 +0000

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
 	if (*head == NULL)
 		return;
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
-		free((*head)->next);
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
-	free(*head);
-	*head = NULL;
 }
 
