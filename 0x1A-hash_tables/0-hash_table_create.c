d1c8d331c30a2b98cdbf1a6c217660493cea9f41
boushra-chl <bchilouch1998@gmail.com>
2024-02-18 17:08:38 +0000

    betty 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return(NULL);
+		return (NULL);
 	table->size = size;
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
-	return(table);
+	return (table);
 }
